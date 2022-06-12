576a74bbdfa80891b4f748d3dc54a5819766fa23
Gabriel Hung <ghung392@live.ca>
2022-06-11 17:49:20 -0700

    Added info to info page

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -9,9 +9,16 @@ type Props = {
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+      <div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Welcome to Weddle!<br/><br/>
+      </p>
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Guess the word in 6 tries. After each guess, the colour of the tiles will
+        change to show how close your guess was to the word. Keep in mind that unlike Wordle, 
+        the words in this Weddle are tied to Crystal and Josh just like the knot that was tied earlier! 
+        They may even be words that don't exist in normal dictionaries!
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -57,15 +64,28 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         The letter U is not in the word in any spot.
       </p>
 
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+       <br/><br/> The better you do, the better the kiss Crystal and Josh will do! 
+        Feel free to help one another at your table. The more kisses the merrier! 
+        Below are the following kisses you can win based on the number of guesses it took you to guess the secret word.<br/><br/>
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        1 - Choose ANY kiss
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        2 - Hold a kiss for 10 seconds
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        3 - Jumping into one's arms
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        4 - The Dip
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        5 - Behind the back
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        6 - Just a peck
       </p>
     </BaseModal>
   )
